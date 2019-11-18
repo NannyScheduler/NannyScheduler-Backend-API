c97ab6e664b45398c80a7c86b15ea8d94323e18e
mogwai <feelirabs@gmail.com>
2019-11-18 04:51:07 +0100

    Installed express

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-require('dotenv').config()
+require('dotenv').config();
+
+const server = require('express');
+
+server.use(express.json());
+
+const port = 9000;
+
+server.length('/', (req, res) => {
+    res.status(201).send("Hello")
+})
+
+server.listen(port, ()=>console.log("The server is alive!"));
