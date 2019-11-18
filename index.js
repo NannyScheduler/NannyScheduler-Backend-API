7706321a3b0b5b93bdde261a885f4d36f76e75fb
mogwai <feelirabs@gmail.com>
2019-11-18 05:20:02 +0100

    created model/helpers

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const port = 9000;
 
 server.get('/', (req, res) => {
     res.status(201).send('<img src="https://media.giphy.com/media/UqUJhrD0om73q/giphy.gif"/>')
+});
+
+server.get('/nannies', (req, res) => {
+    
 })
 
 server.listen(port, ()=>console.log("The server is alive!"));
